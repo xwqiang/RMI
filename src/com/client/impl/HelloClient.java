6408168d26ae459f3ee8d45864e5abc9e84c406d
xwqiang <642143714@qq.com>
2014-05-06 18:41:59 +0800

    init

---
+++ src/com/client/impl/HelloClient.java
@@ -0,0 +1,27 @@
+package com.client.impl;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+import com.rmi.IHello;
+
+public class HelloClient { 
+    public static void main(String args[]){ 
+        try { 
+            //在RMI服务注册表中查找名称为RHello的对象，并调用其上的方法 
+        	
+            IHello rhello =(IHello) Naming.lookup("rmi://localhost:8888/RHello"); 
+            System.out.println(rhello.helloWorld()); 
+            System.out.println(rhello.sayHelloToSomeBody("熔岩")); 
+            System.out.println(rhello.getUser().getUname());
+        } catch (NotBoundException e) { 
+            e.printStackTrace(); 
+        } catch (MalformedURLException e) { 
+            e.printStackTrace(); 
+        } catch (RemoteException e) { 
+            e.printStackTrace();   
+        } 
+    } 
+}
---
+++ src/com/model/User.java
@@ -0,0 +1,27 @@
+package com.model;
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+	int id;
+	String uname;
+	String passwd;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+}
